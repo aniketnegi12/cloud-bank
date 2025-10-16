6b5e667421f56d164544f470513e446747c37ba1
aniketnegi12 <aniketnegi693@gmail.com>
2025-10-16 16:36:40 +0530

    Delete firebase_config.py

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase with your service account
-cred = credentials.Certificate("bank-management-system-a0944-firebase-adminsdk-fbsvc-290ad1ae1a.json")
-firebase_admin.initialize_app(cred)
-
-# Create Firestore client
-db = firestore.client()
